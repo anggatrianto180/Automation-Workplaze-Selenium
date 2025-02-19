18cf4d1a863eac8552b81fa964957c8eab424c26
Angga <anggatrianto180@gmail.com>
2025-02-20 00:05:02 +0700

    push

---
+++ src/test/java/Workplaze/Login.java
@@ -0,0 +1,59 @@
+package Workplaze;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Login {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        // Tentukan lokasi chromedriver
+        System.setProperty("webdriver.chrome.driver", "H:\\Dataon\\Automation\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        // Go to Login Page
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://sfworkplaze.dataon.com"); // Ganti dengan URL yang sesuai
+    }
+
+    @Test
+    private void checkElement() throws InterruptedException {
+        WebElement usernameField = driver.findElement(By.xpath("//input[@id='userName']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        WebElement loginButton = driver.findElement(By.xpath("//span[normalize-space()='Login']"));
+
+
+        //Input Username dan password
+        passwordField.sendKeys("password123");
+        Thread.sleep(3000);
+        usernameField.sendKeys("gordon");
+        Thread.sleep(3000);
+        loginButton.click();
+        Thread.sleep(10000);
+
+        WebElement dataElement = driver.findElement(By.xpath("//div[@class='bio-desc-name']"));
+        // Ambil teks dari elemen tersebut
+        String actualData = dataElement.getText();
+        String expectedData = "Gordon Enns";
+        Assert.assertEquals(actualData, expectedData, "Verifikasi data gagal!");
+        Thread.sleep(10000);
+
+
+    }
+
+    @AfterTest
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
